c9e32d52c569cb263050494cda3a4c87b4dd8cb3
Alec Nikolas Reiter <alecreiter@gmail.com>
2018-04-19 22:03:50 -0400

    Initial commit

---
+++ docs/index.rst
@@ -0,0 +1,23 @@
+.. gamenight documentation master file, created by
+   sphinx-quickstart on Thu Apr 19 21:06:02 2018.
+   You can adapt this file completely to your liking, but it should at least
+   contain the root `toctree` directive.
+
+Welcome to gamenight's documentation!
+=====================================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+.. currentmodule:: gamenight
+.. autofunction:: gamenight.some_async_thingy
+.. autofunction:: all_odd
+
+
+Indices and tables
+==================
+
+* :ref:`genindex`
+* :ref:`modindex`
+* :ref:`search`
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    readme = f.read()
+
+with open('CHANGELOG', 'r') as f:
+    changelog = f.read()
+
+
+if __name__ == "__main__":
+    setup(
+        name='gamenight',
+        version='0.0.1',
+        author='Alec Nikolas Reiter',
+        author_email='alecreiter@gmail.com',
+        description='Organize a game night!',
+        long_description=readme + '\n\n' + changelog,
+        license='MIT',
+        packages=find_packages('src', exclude=["test"]),
+        package_dir={'': 'src'},
+        package_data={'': ['LICENSE', 'NOTICE', 'README.rst', 'CHANGELOG']},
+        include_package_data=True,
+        zip_safe=False,
+        url="https://github.com/justanr/gamenight",
+    )
